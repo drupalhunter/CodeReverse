9ec6639d61513266ab03ac9582778e94334235e7
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2014-04-07 12:14:38 +0900

    add coderev-default.h

---
+++ coderev-default.h
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////
+// coderev-default.h - the default parsing file for CodeReverse
+// Copyright (C) 2013-2014 Katayama Hirofumi MZ.  All rights reserved.
+////////////////////////////////////////////////////////////////////////////
+// This file is part of CodeReverse.
+////////////////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <string.h>
+#include <malloc.h>
+#include <memory.h>
+
+#include <ctype.h>
+#include <time.h>
+#include <math.h>
+#include <process.h>
+#include <search.h>
+
+#include <assert.h>
+
+#include <io.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <direct.h>
+
+#include <conio.h>
+#include <signal.h>
+#include <setjmp.h>
+
+#ifdef JAPAN
+    #include <jstring.h>
+    #include <jctype.h>
+#endif
+
+#if 0   // 16-bit
+    #include <dos.h>
+    #include <stdrom.h>
+    #include <machine.h>
+    #include <fcntl.h>
+#endif
+
+// Windows related
+#include <windows.h>
+#include <commdlg.h>
+#include <commctrl.h>
+#include <mmsystem.h>
+#include <shellapi.h>
+#include <vfw.h>
+#include <tlhelp32.h>
+#include <shlwapi.h>
+
+////////////////////////////////////////////////////////////////////////////
