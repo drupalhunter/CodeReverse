03a3f05e78db42461123d5b46b2c6e1f31393210
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2014-05-24 15:32:08 +0900

    Add asmpat32.dat and asmpat64.dat

--- CodeReverse.cpp
+++ CodeReverse.cpp
@@ -14,15 +14,15 @@ const char * const cr_logo =
     "///////////////////////////////////////////////\n"
 #ifdef _WIN64
 # ifdef __GNUC__
-    "// CodeReverse 0.1.4 (64-bit) for gcc        //\n"
+    "// CodeReverse 0.1.5 (64-bit) for gcc        //\n"
 # elif defined(_MSC_VER)
-    "// CodeReverse 0.1.4 (64-bit) for cl         //\n"
+    "// CodeReverse 0.1.5 (64-bit) for cl         //\n"
 # endif
 #else   // ndef _WIN64
 # ifdef __GNUC__
-    "// CodeReverse 0.1.4 (32-bit) for gcc        //\n"
+    "// CodeReverse 0.1.5 (32-bit) for gcc        //\n"
 # elif defined(_MSC_VER)
-    "// CodeReverse 0.1.4 (32-bit) for cl         //\n"
+    "// CodeReverse 0.1.5 (32-bit) for cl         //\n"
 # endif
 #endif  // ndef _WIN64
     "// https://github.com/katahiromz/CodeReverse //\n"
